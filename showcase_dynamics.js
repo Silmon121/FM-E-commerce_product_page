1f31e1bf2709d74caad474da6fad2310ad75d8e8
Silmon121 <simonmandak@seznam.cz>
2025-01-19 14:47:55 +0100

    More dynamics
    Added:
    - menu resposivity (hover over li)
    - profile icon hover border

--- showcase_dynamics.js
+++ showcase_dynamics.js
@@ -17,7 +17,6 @@ second_thumbnail_img.addEventListener('click', e => {
     selected_thumbnail_img = second_thumbnail_img;
     main_img.src = "images/image-product-2.jpg";
     second_thumbnail_img.classList.add('active');
-
 })
 third_thumbnail_img.addEventListener('click', e => {
     selected_thumbnail_img.classList.remove('active');
