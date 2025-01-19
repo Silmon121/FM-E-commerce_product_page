15ab566c0b4215260015aa3a0ceecbf84c8f1f86
Silmon121 <simonmandak@seznam.cz>
2025-01-19 14:11:47 +0100

    Showcase dynamics
    Added:
    - Dynamics JS file
    - Functional showcase bar

---
+++ dynamics.js
@@ -0,0 +1,34 @@
+const first_thumbnail_img = document.getElementById('thumbnail1');
+const second_thumbnail_img = document.getElementById("thumbnail2");
+const third_thumbnail_img = document.getElementById("thumbnail3");
+const fourth_thumbnail_img = document.getElementById("thumbnail4");
+const main_img = document.getElementById("main_img");
+
+let selected_thumbnail_img = first_thumbnail_img;
+
+first_thumbnail_img.addEventListener('click', e => {
+    selected_thumbnail_img.classList.remove('active');
+    selected_thumbnail_img = first_thumbnail_img;
+    main_img.src = "images/image-product-1.jpg";
+    first_thumbnail_img.classList.add('active');
+})
+second_thumbnail_img.addEventListener('click', e => {
+    selected_thumbnail_img.classList.remove('active');
+    selected_thumbnail_img = second_thumbnail_img;
+    main_img.src = "images/image-product-2.jpg";
+    second_thumbnail_img.classList.add('active');
+
+})
+third_thumbnail_img.addEventListener('click', e => {
+    selected_thumbnail_img.classList.remove('active');
+    selected_thumbnail_img = third_thumbnail_img;
+    main_img.src = "images/image-product-3.jpg";
+    third_thumbnail_img.classList.add('active');
+
+})
+fourth_thumbnail_img.addEventListener('click', e => {
+    selected_thumbnail_img.classList.remove('active');
+    selected_thumbnail_img = fourth_thumbnail_img;
+    main_img.src = "images/image-product-4.jpg";
+    fourth_thumbnail_img.classList.add('active');
+})
